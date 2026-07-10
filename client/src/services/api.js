bfa37b312197305e09666915e6ed0439de1cc715
DAMINI REDDY <daminipakkiru@gmail.com>
2026-07-10 12:37:09 +0530

    Update api.js

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios"
 
 const API = axios.create({
-  baseURL: "https://campustrace-s23e.onrender.com",
+  baseURL: "https://campustrace-s23e.onrender.com/api",
 })
 
 export default API
